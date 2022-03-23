c2858c4d8cb7c0cc68cba5c0370ceee93ae055be
FranDad <francisco.jimenez@externos-cl.cencosud.com>
2022-03-23 15:07:32 -0300

    telnet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const app = require('./app')
-const port = process.env.PORT || 3000
-app.listen(port,() => {
-    console.log(`server is runnig in port ${port}`);
-})
+const telnet = require('telnet-client');
+const server = new telnet();
+
+// display server response
+server.on("data", function(data){
+    console.log(''+data);
+});
+
+// login when connected
+server.on("connect", function(){
+    server.write("login <user> <pass>");
+});
+
+// connect to server
+server.connect({
+    host: "172.16.0.1",
+    port: 9600
+});

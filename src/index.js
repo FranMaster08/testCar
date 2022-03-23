c4a91ed329b7e4c9b52f6eb3ca1609b53607a1bb
FranDad <francisco.jimenez@externos-cl.cencosud.com>
2022-03-23 15:23:30 -0300

    data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const telnet = require('telnet-client');
-const server = new telnet();
+const { Telnet } = require('telnet-client')
+const connection = new Telnet()
 
-// display server response
-server.on("data", function(data){
-    console.log(''+data);
-});
+// these parameters are just examples and most probably won't work for your use-case.
+const params = {
+  host: '127.0.0.1',
+  port: 23,
+  shellPrompt: '/ # ', // or negotiationMandatory: false
+  timeout: 1500
+}
 
-// login when connected
-server.on("connect", function(){
-    server.write("login <user> <pass>");
-});
-
-// connect to server
-server.connect({
-    host: "172.16.0.1",
-    port: 9600
-});
+connection.connect(params)
+  .then(prompt => {
+    connection.exec(cmd)
+    .then(res => {
+      console.log('promises result:', res)
+    })
+  }, error => {
+    console.log('promises reject:', error)
+  })
+  .catch(error => {
+    // handle the throw (timeout)
+  })
